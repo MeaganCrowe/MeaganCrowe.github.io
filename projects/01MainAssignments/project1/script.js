ae241ea9dda82974d2aa4ade856d6fc343d80c4a
MeaganCrowe <meagancrowe.dej@gmail.com>
2023-12-08 16:48:54 +0100

    Creative Coding 1&2

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,29 +1,181 @@
-let len = 10;
-let bg;
+let boxSize = 250;
+let insert = "A";
+let font;
+let textleroy;
+let textluke;
+const t = 200;
 
+// coolors color palette
+const coolorsPalette = [
+  '#eb300f', '#fe7688', '#fff566', '#212121', '#306e42', '#0d3b66'
+];
+
+
+
+// preload font
+function preload() {
+  font = loadFont('SpaceMono-Regular.otf');
+}
+
+// set up graphic (textleroy is the name of the graphic)
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(10, 200, 58);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  background(0);
+  textleroy = createGraphics(200, 200);
+  textluke = createGraphics(windowWidth + 600, windowHeight + 300);
+  createPattern();
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(20,250, 40);
-	
-	rect(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-	rect(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(89, 30, 33);
-	}
+function draw() {
+  background(252, 191, 73);
+  push();
+  translate(0, 0, -200);
+  imageMode(CENTER);
+  image(textluke, 0, 0);
+  pop();
+  noStroke();
+
+  // rotate boxy box speed
+  rotateX(frameCount * 0.005);
+  rotateY(frameCount * 0.005);
+
+  // box creation
+  drawBox();
 }
 
+function drawBox() {
+  texture(textleroy);
+  box(boxSize);
+}
+
+// one letter per type
+function keyTyped() {
+  insert = key;
+  insert = insert.toUpperCase();
+  createPattern();
+}
+
+function createPattern() {
+
+
+  // Generate a new set of random colors
+  const randomColors = getRandomColors();
+
+  // Select pattern based on the key pressed
+  let selectedPattern  = PTN.checked(t / 8);
+  switch (insert) {
+//patterns
+    case 'A':
+      selectedPattern = PTN.stripePolygon(4, t / 12, t / 6);
+      break;
+    case 'S':
+      selectedPattern = PTN.stripePolygon(6, t / 12);
+      break;
+    case 'D':
+      selectedPattern = PTN.stripeRadial(TAU / 30);
+      break;
+    case 'F':
+      selectedPattern = PTN.wave(t / 2, t / 10, t / 5, t / 10);
+      break;
+    case 'G':
+      selectedPattern = PTN.dot(t / 10, t / 20);
+      break;
+    case 'H':
+      selectedPattern = PTN.checked(t / 8, t / 3);
+      break;
+    case 'J':
+      selectedPattern = PTN.cross(t / 10, t / 40);
+      break;
+    case 'K':
+      selectedPattern = PTN.triangle(t / 5, t / 10);
+      break;
+//patterns
+    case 'L':
+      selectedPattern = PTN.stripePolygon(4,t / 2, t/7);
+      break;
+    case 'Z':
+      selectedPattern = PTN.stripePolygon(6, t / 12);
+      break;
+    case 'X':
+      selectedPattern = PTN.stripeRadial(TAU / 30);
+      break;
+    case 'C':
+      selectedPattern = PTN.wave(t / 2, t / 10, t / 5, t / 10);
+      break;
+    case 'V':
+      selectedPattern = PTN.dot(t / 10, t / 20);
+      break;
+    case 'B':
+      selectedPattern = PTN.checked(t / 8, t / 3);
+      break;
+    case 'N':
+      selectedPattern = PTN.cross(t / 10, t / 40);
+      break;
+    case 'M':
+      selectedPattern = PTN.triangle(t / 5, t / 10);
+      break;
+//patterns
+    case 'Q':
+      selectedPattern = PTN.stripePolygon(4, t / 12, t / 6);
+      break;
+    case 'W':
+      selectedPattern = PTN.stripePolygon(6, t / 12);
+      break;
+    case 'E':
+      selectedPattern = PTN.stripeRadial(TAU / 30);
+      break;
+    case 'R':
+      selectedPattern = PTN.wave(t / 2, t / 10, t / 5, t / 10);
+      break;
+    case 'T':
+      selectedPattern = PTN.dot(t / 10, t / 20);
+      break;
+		case 'Y':
+      selectedPattern = PTN.checked(t / 8, t / 3);
+      break;
+		case 'U':
+      selectedPattern = PTN.cross(t / 10, t / 40);
+      break;
+		case 'I':
+      selectedPattern = PTN.triangle(t / 5, t / 10);
+      break;
+//patterns
+		case 'O':
+      selectedPattern = PTN.stripePolygon(4, t / 12, t / 6);
+      break;
+		case 'P':
+      selectedPattern = PTN.stripePolygon(6, t / 12);
+      break;
+    default:
+      selectedPattern = PTN.stripeRadial(TAU / 30); // Default pattern
+  }
+
+  // text texture hehe
+  textluke.patternColors(randomColors);
+  textluke.background(0);
+	textluke.pattern(selectedPattern);
+  //selectedPattern(textluke, t); // Apply the selected pattern
+  textluke.rectPattern(0, 0, windowWidth + 600, windowHeight + 300);
+
+  textleroy.patternColors(randomColors);
+  textleroy.background(255, 20);
+  PTN.checked(textleroy, t / 1); // Use a specific pattern for textleroy
+  textleroy.rectPattern(0, 0, t, t);
+
+  textleroy.fill(255);
+  textleroy.textFont(font);
+  textleroy.textSize(200);
+  textleroy.textAlign(CENTER, CENTER);
+  textleroy.text(insert, 100, 55);
+}
+
+function getRandomColors() {
+  // Shuffle the color palette to get a random order
+  const shuffledColors = coolorsPalette.slice();
+  for (let i = shuffledColors.length - 1; i > 0; i--) {
+    const j = floor(random(i + 1));
+    [shuffledColors[i], shuffledColors[j]] = [shuffledColors[j], shuffledColors[i]];
+  }
+  // Return the first three colors (adjust as needed)
+  return shuffledColors.slice(0, 3);
+}
--- projects/01MainAssignments/project2/script.js
+++ projects/01MainAssignments/project2/script.js
@@ -1,29 +1,99 @@
-let len = 10;
-let bg;
+let toggleSwitch;
+let spinning = false;
+let flyingOut = false;
+let flyingBack = false;
+let backgroundColor;
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(200, 0, 58);
+  createCanvas(windowWidth, windowHeight);
+  backgroundColor = color(200);
+  toggleSwitch = new ToggleSwitch(width / 2, height / 2, 60);
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(0,130, 140);
-	
-	ellipse(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-  	ellipse(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(189, 3, 33);
-	}
+function draw() {
+  background(backgroundColor);
+  toggleSwitch.update();
+  toggleSwitch.display();
+}
+
+function mousePressed() {
+  if (toggleSwitch.contains(mouseX, mouseY)) {
+    toggleSwitch.toggle();
+    flyingOut = true;
+    backgroundColor = toggleSwitch.isOn ? color(150, 200, 255) : color(200);
+  } else {
+    flyingBack = true;
+  }
+}
+
+function mouseMoved() {
+  if (toggleSwitch.contains(mouseX, mouseY)) {
+    spinning = true;
+  } else {
+    spinning = false;
+  }
+}
+
+class ToggleSwitch {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.isOn = false;
+    this.rotation = 0;
+    this.flySpeed = 5;
+  }
+
+  toggle() {
+    this.isOn = !this.isOn;
+  }
+
+  contains(px, py) {
+    return (
+      px > this.x - this.size / 2 &&
+      px < this.x + this.size / 2 &&
+      py > this.y - this.size / 4 &&
+      py < this.y + this.size / 4
+    );
+  }
+
+  update() {
+    if (spinning) {
+      this.rotation += 0.5;
+    }
+
+    if (flyingOut) {
+      this.x += this.flySpeed;
+      if (this.x > width) {
+        flyingOut = false;
+        flyingBack = true;
+        this.x = -this.size; // Start flying back from the left side
+      }
+    }
+
+    if (flyingBack) {
+      this.x += this.flySpeed;
+      if (this.x > width / 2) {
+        flyingBack = false;
+      }
+    }
+  }
+
+  display() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.rotation);
+    fill(255);
+    noStroke();
+    rectMode(CENTER);
+    rect(0, 0, this.size, this.size / 2, 10);
+
+    fill(this.isOn ? color(150, 200, 255) : color(200));
+    noStroke();
+    ellipseMode(CENTER);
+    let handleX = this.isOn ? this.size / 4 : -this.size / 4;
+    ellipse(handleX, 0, this.size / 3);
+    pop();
+  }
 }
 
